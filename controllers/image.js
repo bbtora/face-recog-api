bf2e90532ac4575e9666b24a8bf0cf94917399bb
bbtora <23010526+bbtora@users.noreply.github.com>
2018-12-08 14:25:08 +0530

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 const handleApi = (req, res) => {
     app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => console.log(res.json(data)))
+    .then(data => (res.json(data))
     .catch(err => res.status(400).json('unable to work with API'));
 }
 
